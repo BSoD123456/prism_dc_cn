6b9184b53d1e453c9c8e567711c80333a19fa903
tringcooler <tringcooler@users.noreply.github.com>
2026-04-07 02:09:48 +0800

    Update scode.py

--- scode.py
+++ scode.py
@@ -7,11 +7,54 @@ from report import report
 class err_scode_syntax(ValueError):
     pass
 
+class c_scode_buf:
+
+    IDTSYM = '    '
+
+    def __init__(self, parent, touched):
+        self.par = parent
+        self.tch = touched
+        self.idt = 0
+        self.buf = []
+
+    def indent(self, val):
+        self.idt += val
+        if self.idt < 0:
+            raise err_scode_syntax('indent underflow')
+
+    def _idtsym(self):
+        return (self.IDTSYM for _ in range(self.idt))
+
+    def _write_par(self, line):
+        self.par.write(line)
+
+    def write(self, s):
+        line = ''.join((*self._idtsym(), s))
+        if self.tch:
+            self._write_par(line)
+        else:
+            self.buf.append(line)
+
+    def touch(self):
+        if self.tch:
+            return
+        for line in self.buf:
+            self._write_par(line)
+
+class c_scode_buf_std(c_scode_buf):
+
+    def __init__(self):
+        super().__init__(None, True)
+
+    def _write_par(self, line):
+        print(line)
+
 class c_scode_program:
 
     def __init__(self, ast):
         self.ast = ast
         self.chrset = c_charset_jp()
+        self.buf = c_scode_buf_std()
 
     def _gen_anode(self, nd, assume = None, ctx = None):
         cn = nd.__class__.__name__
@@ -41,7 +84,7 @@ class c_scode_program:
         for c in nd.text:
             txts.append(self.chrset.dec(c))
         txt = ''.join(txts)
-        print(f'{nd.name}: {txt}')
+        self.buf.write(f'{nd.name}: {txt}')
 
     def gen_code(self):
         self._gen_anode(self.ast)
