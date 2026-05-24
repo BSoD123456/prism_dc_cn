83cd1e49adbccdf770dfda3534c019809412aaa5
tringcooler <tringcooler@users.noreply.github.com>
2026-05-25 02:38:45 +0800

    Update modzh_ipt.py

--- modzh_ipt.py
+++ modzh_ipt.py
@@ -7,6 +7,8 @@ import re
 
 def modzh_ipt(sfd, dfd, mfd):
     seltab = {}
+    tabidx = 0
+    lstintab = False
     sln = 0
     patt = r'(?<![^\x00-\x7f])([^\x00-\x7f])(?![^\x00-\x7f])'
     while True:
@@ -16,7 +18,8 @@ def modzh_ipt(sfd, dfd, mfd):
         if not sline:
             assert not dline
             break
-        if sln < 57090:
+        assert dline
+        if sln < 57090 or dline == '\n':
             mfd.write(dline)
             continue
         sseq = re.split(patt, sline)
@@ -32,17 +35,25 @@ def modzh_ipt(sfd, dfd, mfd):
                 if m:
                     selidx = int(m.group(1), 16)
             else:
-                if selidx is None:
-                    if not sv in seltab:
-                        raise ValueError(f'unref char: {sv}')
-                    mchar = seltab[sv][1]
-                else:
+                if isinstance(selidx, int):
                     if sv in seltab:
                         raise ValueError(f'duplicate char: {sv}')
                     mchar = '？'
-                    seltab[sv] = [selidx, mchar]
+                    seltab[sv] = (selidx, tabidx, mchar)
+                else:
+                    if not sv in seltab:
+                        raise ValueError(f'unref char: {sv}')
+                    mchar = seltab[sv][-1]
+                    selidx = False
                 mseq.append(mchar)
+        if selidx is False:
+            if lstintab:
+                tabidx += 1
+            lstintab = False
+        elif not selidx is None:
+            lstintab = True
         mfd.write(''.join(mseq))
+    return seltab, tabidx
 
 if __name__ == '__main__':
     import pdb
@@ -51,8 +62,12 @@ if __name__ == '__main__':
     ppr = lambda *a, **ka: pprint(*a, **ka, sort_dicts = False)
 
     def main():
+        global cs, st
         with open(r'wktab\dialog_done2.txt', 'r', encoding = 'utf-8') as sfd:
             with open(r'trans\dialog_zh.txt', 'r', encoding = 'utf-8') as dfd:
                 with open(r'trans\dialog_zh_mod_ipt.txt', 'w', encoding = 'utf-8') as mfd:
-                    modzh_ipt(sfd, dfd, mfd)
+                    st, tn = modzh_ipt(sfd, dfd, mfd)
+        cs = []
+        for i in range(tn):
+            cs.append([c for c, (_, t, v) in st.items() if t == i])
     main()
