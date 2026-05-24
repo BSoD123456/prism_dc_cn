d27639ee5a0f001f93782ffaf33cf3328d285238
tringcooler <tringcooler@users.noreply.github.com>
2026-05-25 04:53:03 +0800

    Update maker.py

--- maker.py
+++ maker.py
@@ -31,9 +31,15 @@ class c_maker_rule:
             if (mth.__code__.co_flags & 0xc) != 0:
                 self._error(f'invalid mk method: {mn}')
             rnum = mth.__code__.co_argcount - 1 # with self
-            if rnum > len(reqs):
-                self._error(f'require {rnum - len(reqs)} more items')
-            r = mth(*reqs[:rnum])
+            dreqs = []
+            rlen = len(reqs)
+            for i in range(rnum):
+                if i < rlen:
+                    d = reqs[i]
+                else:
+                    d = None
+                dreqs.append(d)
+            r = mth(*dreqs)
             if not r is None:
                 return r
         self._error(f'nothing made')
@@ -73,37 +79,47 @@ class c_maker_rule_vir(c_maker_rule):
     def mk0(self):
         return True
 
+class c_maker_rule_path(c_maker_rule):
+
+    def mk0(self, path):
+        dpath = self.name
+        if not path is None:
+            dpath = os.path.join(path, dpath)
+        self.path = dpath
+
+class c_maker_rule_dir(c_maker_rule_path):
+
+    def mk0(self, path):
+        super().mk0(path)
+        fn = self.path
+        if os.path.isdir(fn):
+            return fn
+        elif os.path.exists(fn):
+            return None
+        os.makedirs(fn)
+        return fn
+
 class c_maker_rule_rawfile(c_maker_rule):
 
-    def mk0(self):
-        fn = self.name
+    def mk0(self, path):
+        super().mk0(path)
+        fn = self.path
         if not os.path.isfile(fn):
             return None
         with open(fn, 'rb') as fd:
             return fd.read()
 
-class c_maker_rule_dir(c_maker_rule):
-
-    def mk0(self):
-        fn = self.name
-        if os.path.isdir(fn):
-            return True
-        elif os.path.exists(fn):
-            return None
-        os.makedirs(fn)
-        return True
-
 def make_all(paths):
     rules = {}
     rules.update({
-        path: (c_maker_rule_dir,),
+        path: (c_maker_rule_dir,)
         for path in paths.values()
     })
     rules.update({
-        'all': (c_maker_rule_vir, wkpath),
+        
     })
     rules.update({
-        'all': (c_maker_rule_vir, wkpath),
+        'all': (c_maker_rule_vir, paths['work'], paths['extract']),
     })
     mkr = c_maker(rules)
     mkr.make('all')
@@ -114,6 +130,11 @@ if __name__ == '__main__':
     from pprint import pprint
     ppr = lambda *a, **ka: pprint(*a, **ka, sort_dicts = False)
 
+    PATHS = {
+        'work': 'wktab2/work',
+        'extract': 'wktab2/extract',
+    }
+
     def main():
-        make_all('wktab2')
+        make_all(PATHS)
     main()
