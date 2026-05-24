3ee4ba520fa09b96a9bc5abf85f76d086ca9ef3a
tringcooler <tringcooler@users.noreply.github.com>
2026-05-25 02:02:44 +0800

    Update modzh_ipt.py

--- modzh_ipt.py
+++ modzh_ipt.py
@@ -5,7 +5,8 @@ from charset import _get_gbk_char_type
 
 import re
 
-def modzh(sfd, dfd, mfd):
+def modzh_ipt(sfd, dfd, mfd):
+    seltab = {}
     sln = 0
     patt = r'(?<![^\x00-\x7f])([^\x00-\x7f])(?![^\x00-\x7f])'
     while True:
@@ -23,13 +24,24 @@ def modzh(sfd, dfd, mfd):
         if not len(sseq) == len(dseq):
             raise ValueError(f'unmatched line: {dline}')
         mseq = []
+        selidx = None
         for i, (sv, dv) in enumerate(zip(sseq, dseq)):
             if i % 2 == 0:
                 mseq.append(dv)
+                m = re.match(r'.*\[SEL\:([0-9a-fA-F])+\]', sv)
+                if m:
+                    selidx = int(m.group(1), 16)
             else:
-                if sv != dv:
-                    raise ValueError(f'unmatched key char: {sv}/{dv}')
-                mseq.append('？')
+                if selidx is None:
+                    if not sv in seltab:
+                        raise ValueError(f'unref char: {sv}')
+                    mchar = seltab[sv][1]
+                else:
+                    if sv in seltab:
+                        raise ValueError(f'duplicate char: {sv}')
+                    mchar = '？'
+                    seltab[sv] = [selidx, mchar]
+                mseq.append(mchar)
         mfd.write(''.join(mseq))
 
 if __name__ == '__main__':
@@ -42,5 +54,5 @@ if __name__ == '__main__':
         with open(r'wktab\dialog_done2.txt', 'r', encoding = 'utf-8') as sfd:
             with open(r'trans\dialog_zh.txt', 'r', encoding = 'utf-8') as dfd:
                 with open(r'trans\dialog_zh_mod_ipt.txt', 'w', encoding = 'utf-8') as mfd:
-                    modzh(sfd, dfd, mfd)
+                    modzh_ipt(sfd, dfd, mfd)
     main()
